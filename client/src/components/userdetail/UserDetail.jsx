57b699fd7b00d98dce87115fb8ac911f383e05be
JewelAlex21 <jewelalex2103@gmail.com>
2024-02-11 15:33:09 +0530

    final front design commit

--- client/src/components/userdetail/UserDetail.jsx
+++ client/src/components/userdetail/UserDetail.jsx
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./userdetail.css";
-import config from '../../environment/config';
+import config from "../../environment/config";
 const UserDetail = ({ accountId }) => {
   const [user, setUser] = useState(null);
-  const apiURL =  config.apiUrl
+  const apiURL = config.apiUrl;
 
   useEffect(() => {
     const fetchUserDetails = async () => {
       try {
-        const response = await fetch(
-          `${apiURL}/api/user/${accountId}`
-        );
+        const response = await fetch(`${apiURL}/api/user/${accountId}`);
         const userData = await response.json();
         setUser(userData);
       } catch (error) {
@@ -22,15 +20,24 @@ const UserDetail = ({ accountId }) => {
   }, [accountId]);
 
   return (
-    <div className="container">
+    <div className="container-fluid min-vh-100">
       {user ? (
-        <div>
-          <h2>User Details</h2>
-          <p>Name: {user.fullName}</p>
-          <p>Email: {user.email}</p>
-          <p>Phone Number: {user.phoneNumber}</p>
-          <p>Date Of Birth: {new Date(user.dob).toLocaleDateString()}</p>{" "}
-          <p>Additional Savings/Investments: {user.investments ? user.investments : '-'}</p>
+        <div className="row justify-content-center min-vh-100">
+          <div className="col-md-4 align-self-center">
+            <div className="text-center mb-4">
+              <h2>User Details</h2>
+              <p>Name: <b>{user.fullName}</b></p>
+              <p>Email: <b>{user.email}</b></p>
+              <p>Phone Number: <b>{user.phoneNumber}</b> </p>
+              <p>Date Of Birth: <b>{new Date(user.dob).toLocaleDateString()}</b></p>
+              <p>About Yourself: <b>{user.tellUs ? user.tellUs : "-"}</b></p>
+              <p>Employee Status: <b>{new Date(user.dob).toLocaleDateString()}</b></p>
+              <p>
+                Additional Savings/Investments:
+                <b>{user.investments ? user.investments : "-"}</b>
+              </p>
+            </div>
+          </div>
         </div>
       ) : (
         <p>Loading user details...</p>
--- client/src/components/userform/UserForm.jsx
+++ client/src/components/userform/UserForm.jsx
@@ -3,11 +3,10 @@ import { useDispatch } from "react-redux";
 import { fetchUserSuccess, fetchUserFailure } from "../../redux/actions";
 import UserDetail from "../userdetail/UserDetail";
 import "./userform.css";
-import config from '../../environment/config';
-
+import config from "../../environment/config";
 
 const UserForm = () => {
-  const apiURL = config.apiUrl
+  const apiURL = config.apiUrl;
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -33,6 +32,7 @@ const UserForm = () => {
     tellUs: "",
   });
   const [currentStep, setCurrentStep] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
   const [formSubmitted, setFormSubmitted] = useState(false);
   const [accountId, setAccountId] = useState(null);
@@ -47,6 +47,7 @@ const UserForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     const validationErrors = {};
     if (currentStep === 1) {
       if (!validateEmail(formData.email)) {
@@ -95,8 +96,11 @@ const UserForm = () => {
       } catch (error) {
         setApiError(error.message);
         dispatch(fetchUserFailure(error.message));
+      } finally {
+        setIsLoading(false);
       }
     } else {
+      setIsLoading(false);
       setCurrentStep(currentStep + 1);
     }
   };
@@ -162,170 +166,283 @@ const UserForm = () => {
   }
 
   return (
-    <div className="container">
-      <header>{apiError && <p className="error-message">{apiError}</p>}</header>
+    <div className="container-fluid min-vh-100">
       <main>
-        <form className="form" onSubmit={handleSubmit}>
-          {currentStep === 1 && (
-            <>
-              <h1>Create your account</h1>
-              <p>Set-up your RentlyPass in as little as 2 minutes.</p>
-              <h3>Contact details</h3>
-              <div className="form-group">
-                <label>Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.email && (
-                  <p className="error-message">{errors.email}</p>
-                )}
-              </div>
-              <div className="form-group">
-                <label>Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.password && (
-                  <p className="error-message">{errors.password}</p>
-                )}
-              </div>
-              <div className="form-group">
-                <label>Confirm Password</label>
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  value={formData.confirmPassword}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.confirmPassword && (
-                  <p className="error-message">{errors.confirmPassword}</p>
-                )}
-              </div>
-              <div className="form-group">
-                <label>Phone Number</label>
-                <input
-                  type="text"
-                  name="phoneNumber"
-                  value={formData.phoneNumber}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.phoneNumber && (
-                  <p className="error-message">{errors.phoneNumber}</p>
-                )}
-              </div>
-              <button className="create-btn" type="submit">
-                Save and continue
-              </button>
-            </>
-          )}
+        <div className="row justify-content-center min-vh-100">
+          <div className="col-md-4 align-self-center">
+            <form className="form" onSubmit={handleSubmit}>
+              {currentStep === 1 && (
+                <>
+                  <div className="text-center mb-4">
+                    <h2>Create Your Account</h2>
+                    <p>Set-up ypur Rently Pass as in as little 2 minutes</p>
+                  </div>
+                  <div className="row g-1">
+                    <div className="form-heading">
+                      <h4>Contact Details</h4>
+                    </div>
+                    <div className="form-floating mb-3">
+                      <input
+                        className="form-control"
+                        id="floatingInput"
+                        type="email"
+                        name="email"
+                        placeholder="abc@gmail.com"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                      />
+                      <label htmlFor="floatingInput">Email address</label>
+                      {errors.email && (
+                        <p className="error-message">{errors.email}</p>
+                      )}
+                    </div>
+                    <div className="form-floating mb-3">
+                      <input
+                        className="form-control"
+                        type="text"
+                        id="floatingMobile"
+                        name="phoneNumber"
+                        placeholder="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleChange}
+                        required
+                      />
+                      <label htmlFor="floatingMobile">Mobile Number</label>
+                      {errors.phoneNumber && (
+                        <p className="error-message">{errors.phoneNumber}</p>
+                      )}
+                    </div>
+                    <div className="form-heading">
+                      <h4>Set a Password</h4>
+                    </div>
+                    <div className="form-floating mb-3">
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="floatingPassword"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        placeholder="Password"
+                        required
+                      />
+                      <label htmlFor="floatingPassword">Password</label>
+                      {errors.password && (
+                        <p className="error-message">{errors.password}</p>
+                      )}
+                    </div>
+                    <div className="form-floating">
+                      <input
+                        type="password"
+                        className="form-control"
+                        name="confirmPassword"
+                        id="floatingConfirm"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        placeholder="Password"
+                        required
+                      />
+                      <label htmlFor="floatingConfirm">Confirm Password</label>
+                      {errors.confirmPassword && (
+                        <p className="error-message">
+                          {errors.confirmPassword}
+                        </p>
+                      )}
+                    </div>
+                    <span className="form-paragraph">
+                      We need a password to keep your information safe. But
+                      don’t worry, we’ll also send your custom RentlyPass URL
+                      via email.
+                    </span>
+                    <div className="mt-3">
+                      <button type="submit" className="btn btn-primary w-100">
+                        Create your account
+                      </button>
+                      <header>
+                        {apiError && (
+                          <p className="error-message">{apiError}</p>
+                        )}
+                      </header>
+                    </div>
+                  </div>
+                </>
+              )}
 
-          {currentStep === 2 && (
-            <>
-              <h1>Personal Information</h1>
-              <p>Please answer questions as accurately as possible</p>
-              <div className="form-group">
-                <label>Full Name</label>
-                <input
-                  type="text"
-                  name="fullName"
-                  value={formData.fullName}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.fullName && (
-                  <p className="error-message">{errors.fullName}</p>
-                )}
-              </div>
-              <div className="form-group">
-                <label>Date of Birth</label>
-                <input
-                  type="date"
-                  name="dob"
-                  value={formData.dob}
-                  onChange={handleChange}
-                  required
-                />
-                {errors.dob && <p className="error-message">{errors.dob}</p>}
-              </div>
-              <div className="form-group">
-                <label>How Long have you Lived in this address?</label>
-                <input
-                  type="text"
-                  name="howLong"
-                  value={formData.howLong}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="form-group">
-                <label>Tell us a bit about yourself</label>
-                <textarea
-                  type="text"
-                  name="tellUs"
-                  value={formData.tellUs}
-                  onChange={handleChange}
-                />
-              </div>
-              <button
-                className="back-btn"
-                onClick={() => setCurrentStep(currentStep - 1)}
-              >
-                Back
-              </button>
-              <button className="create-btn" type="submit">
-                Save and continue
-              </button>
-            </>
-          )}
+              {currentStep === 2 && (
+                <>
+                  <div className="text-center mb-4">
+                    <ul className="form-progress mx-auto">
+                      <li className="active">1</li>
+                      <li>2</li>
+                    </ul>
+                    <h2>Personal Information</h2>
+                    <p>Please answer questions as accurately as possible</p>
+                  </div>
+                  <div className="row g-1">
+                    <div className="row g-0 mt-3">
+                      <div className="col-3 pe-3">
+                        <div className="form-floating">
+                          <select
+                            className="form-select"
+                            id="floatingSelect"
+                            aria-label="Floating label select example"
+                          >
+                            <option value="1">Mrs.</option>
+                            <option value="2">Ms.</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="col-9">
+                        <div className="form-floating mb-2">
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="floatingName"
+                            placeholder="fullName"
+                            name="fullName"
+                            value={formData.fullName}
+                            onChange={handleChange}
+                            required
+                          />
+                          <label htmlFor="floatingName">
+                            Full Name as per your passport
+                          </label>
+                          {errors.fullName && (
+                            <p className="error-message">{errors.fullName}</p>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="form-floating mb-2">
+                      <input
+                        type="date"
+                        className="form-control"
+                        name="dob"
+                        id="floatingDob"
+                        value={formData.dob}
+                        onChange={handleChange}
+                        placeholder="Date of birth"
+                        required
+                      />
+                      <label htmlFor="floatingDob">Date of birth</label>
+                      {errors.dob && (
+                        <p className="error-message">{errors.dob}</p>
+                      )}
+                    </div>
+                    <div className="form-floating mb-2">
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="howLong"
+                        id="floatinghowLong"
+                        value={formData.howLong}
+                        onChange={handleChange}
+                        placeholder="How Long"
+                      />
+                      <label htmlFor="floatinghowLong">
+                        How Long have you Lived in this address?
+                      </label>
+                    </div>
+                    <div className="form-floating mb-2">
+                      <textarea
+                        type="text"
+                        className="form-control"
+                        name="tellUs"
+                        id="floatingtellUs"
+                        value={formData.tellUs}
+                        onChange={handleChange}
+                        placeholder="tellUs"
+                      />
+                      <label htmlFor="floatingtellUs">
+                        Tell us a bit about yourself
+                      </label>
+                    </div>
+                    {/* <button
+                      className="back-btn"
+                      onClick={() => setCurrentStep(currentStep - 1)}
+                    >
+                      Back
+                    </button> */}
+                    <div className="mt-3">
+                      <button type="submit" className="btn btn-primary w-100">
+                        Save and continue
+                      </button>
+                    </div>
+                  </div>
+                </>
+              )}
 
-          {currentStep === 3 && (
-            <>
-              <h1>Financial Information</h1>
-              <p>All your information is stored securely.</p>
-              <div className="form-group">
-                <label>Employment Status</label>
-                <input
-                  type="text"
-                  name="employeeStatus"
-                  value={formData.employeeStatus}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="form-group">
-                <label>Additional Savings/Investments</label>
-                <input
-                  type="text"
-                  name="investments"
-                  value={formData.investments}
-                  onChange={handleChange}
-                />
-              </div>
-              <span>
-                All information can be edited once you have created your
-                account.
-              </span>
-              <br />
-              <button
-                className="back-btn"
-                onClick={() => setCurrentStep(currentStep - 1)}
-              >
-                Back
-              </button>
-              <button className="create-btn" type="submit">
-                Create your account
-              </button>
-            </>
-          )}
-        </form>
+              {currentStep === 3 && (
+                <>
+                  <div className="text-center mb-4">
+                    <ul className="form-progress mx-auto">
+                      <li>1</li>
+                      <li className="active">2</li>
+                    </ul>
+                    <h1>Financial Information</h1>
+                    <p>All your information is stored securely.</p>
+                  </div>
+                  <div className="row g-1">
+                    <div className="form-floating mb-2">
+                      <select
+                        className="form-select"
+                        id="floatingStatus"
+                        aria-label="Floating label select example"
+                        value={formData.employeeStatus}
+                        onChange={handleChange}
+                        name="employeeStatus"
+                        placeholder="status"
+                      >
+                        <option value="Worker">Worker</option>
+                        <option value="Employee">Employee</option>
+                        <option value="Self-employed">Self-employed</option>
+                      </select>
+                      <label htmlFor="floatingStatus">
+                        What is your current employment status?
+                      </label>
+                    </div>
+                    <div className="form-floating">
+                      <input
+                        type="text"
+                        className="form-control"
+                        name="investments"
+                        id="floatingInvestments"
+                        value={formData.investments}
+                        onChange={handleChange}
+                        placeholder="investments"
+                      />
+                      <label htmlFor="floatingInvestments">
+                        Additional Savings/Investments
+                      </label>
+                    </div>
+                    <span className="form-paragraph">
+                      All information can be edited once you have created your
+                      account.
+                    </span>
+                    <br />
+                    {/* <button
+                      className="back-btn"
+                      onClick={() => setCurrentStep(currentStep - 1)}
+                    >
+                      Back
+                    </button> */}
+                    <div className="mt-3">
+                      <button type="submit" className="btn btn-primary w-100">
+                        Save and continue
+                      </button>
+                    </div>
+                    {isLoading && (
+                      <div class="spinner-border" role="status">
+                        <span class="sr-only">Loading...</span>
+                      </div>
+                    )}
+                  </div>
+                </>
+              )}
+            </form>
+          </div>
+        </div>
       </main>
     </div>
   );

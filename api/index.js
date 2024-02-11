b6617a63a6ab60fb879a12776174a4ea4e28dd3c
JewelAlex21 <jewelalex2103@gmail.com>
2024-02-11 11:35:30 +0530

    changes in api

--- api/index.js
+++ api/index.js
@@ -3,16 +3,17 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
+const dotenv = require("dotenv")
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+dotenv.config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGO_URL);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
